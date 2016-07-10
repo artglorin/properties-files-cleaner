e0e9d596e72669b8a70e2d98bf044db88a6627e5
V.Verminsky <mail@artglorin.com>
2016-07-10 11:50:12 +0300

    add files encoding checkBox

--- src/main/java/com/artglorin/belprime/clearPropertyApp/controller/MainController.java
+++ src/main/java/com/artglorin/belprime/clearPropertyApp/controller/MainController.java
@@ -3,6 +3,7 @@ package com.artglorin.belprime.clearPropertyApp.controller;
 import com.artglorin.belprime.clearPropertyApp.common.Core;
 import com.artglorin.belprime.clearPropertyApp.utils.FileUtil;
 import javafx.fxml.FXML;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
@@ -16,6 +17,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.Properties;
 
@@ -31,7 +33,14 @@ public class MainController {
     private ListView<File> processedList;
 
     @FXML
+    private ComboBox<Charset> templateEncoding;
+
+    @FXML
+    private ComboBox<Charset> processListEncoding;
+
+    @FXML
     private Label templateLabel;
+
     @FXML
     private TextArea log;
 
@@ -49,6 +58,53 @@ public class MainController {
                     }
                 }
         );
+        final Charset defaultCharset = Charset.forName("UTF-8");
+        final List<Charset> charsetList = Arrays.asList(Charset.forName("IBM00858"),
+                Charset.forName("IBM437"),
+                Charset.forName("IBM775"),
+                Charset.forName("IBM850"),
+                Charset.forName("IBM852"),
+                Charset.forName("IBM855"),
+                Charset.forName("IBM857"),
+                Charset.forName("IBM862"),
+                Charset.forName("IBM866"),
+                Charset.forName("ISO-8859-1"),
+                Charset.forName("ISO-8859-2"),
+                Charset.forName("ISO-8859-4"),
+                Charset.forName("ISO-8859-5"),
+                Charset.forName("ISO-8859-7"),
+                Charset.forName("ISO-8859-9"),
+                Charset.forName("ISO-8859-13"),
+                Charset.forName("ISO-8859-15"),
+                Charset.forName("KOI8-R"),
+                Charset.forName("KOI8-U"),
+                Charset.forName("US-ASCII"),
+                defaultCharset,
+                Charset.forName("UTF-16"),
+                Charset.forName("UTF-16BE"),
+                Charset.forName("UTF-16LE"),
+                Charset.forName("UTF-32"),
+                Charset.forName("UTF-32BE"),
+                Charset.forName("UTF-32LE"),
+                Charset.forName("x-UTF-32BE-BOM"),
+                Charset.forName("x-UTF-32LE-BOM"),
+                Charset.forName("windows-1250"),
+                Charset.forName("windows-1251"),
+                Charset.forName("windows-1252"),
+                Charset.forName("windows-1253"),
+                Charset.forName("windows-1254"),
+                Charset.forName("windows-1257"),
+                Charset.forName("UnicodeBig"),
+                Charset.forName("x-IBM737"),
+                Charset.forName("x-IBM874"),
+                Charset.forName("x-UTF-16LE-BOM")
+        );
+
+        templateEncoding.getItems().addAll(charsetList);
+        templateEncoding.getSelectionModel().select(defaultCharset);
+        processListEncoding.getItems().addAll(charsetList);
+        processListEncoding.getSelectionModel().select(defaultCharset);
+        log.setEditable(false);
     }
 
     @FXML
@@ -77,14 +133,15 @@ public class MainController {
 
     @FXML
     private void process() {
-        final Properties properties = loadProperties(template);
+        final Properties properties = loadProperties(template, templateEncoding.getValue());
+        final Charset value = processListEncoding.getValue();
         processedList.getItems().stream().parallel()
-                .forEach(file -> new Core(properties, file, Charset.forName("Windows-1251")).run());
+                .forEach(file -> new Core(properties, file, value).run());
     }
 
-    private static Properties loadProperties(File template) {
+    private static Properties loadProperties(File template, Charset charset) {
         final Properties properties = new Properties();
-        try (InputStreamReader inputStream = new InputStreamReader(new FileInputStream(template), "Windows-1251")) {
+        try (InputStreamReader inputStream = new InputStreamReader(new FileInputStream(template), charset)) {
             properties.load(inputStream);
         } catch (IOException e) {
             e.printStackTrace();
